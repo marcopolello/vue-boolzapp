8fdddc44a8d4535e574bb4523d654ef94266f8e2
marcopolello <marco-pol@hotmail.it>
2020-11-24 12:51:24 +0100

    cambio struttira dati: msg in un array di oggetti

--- js/script.js
+++ js/script.js
@@ -12,62 +12,216 @@
 var app = new Vue ({
   el: '#app',
   data: {
+    attivo: 0,
     corrispondenze: [
       {
         immagine: "img/avatar_1.jpg",
         nome: "Paolo",
-        messaggi: ["ciao come va?", "tutto bene grazie"],
+        messaggi: [
+          {
+            text: "ciao",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ehila",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "come va?",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: ["ehi, ti disturbo?"],
       },
       {
         immagine: "img/avatar_2.jpg",
         nome: "Luca",
-        messaggi: ["mi annoio", "facciamo qualcosa", "oiiiii"],
+        messaggi: [
+          {
+            text: "oibò",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ciao sono Luca",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "che fai oggi??",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
       },
       {
         immagine: "img/avatar_3.jpg",
         nome: "Marco",
-        messaggi: ["che fai?", "io sono in lavanderia"],
+        messaggi: [
+          {
+            text: "chi sei?",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ciao sono Marco",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "che fai??",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+          {
+            text: "che fai??",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+          {
+            text: "che fai??",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+          {
+            text: "chi sei?",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ciao sono Marco",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ciao sono Marco",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
       },
       {
         immagine: "img/avatar_4.jpg",
         nome: "Michele",
-        messaggi: ["non ci capisco un cazz", "è dura la vita", "ehhhh siiii"],
+        messaggi: [
+          {
+            text: "ciao",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ehila sono Michele",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "che stai facendo??",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
       },
       {
         immagine: "img/avatar_5.jpg",
         nome: "Andrea",
-        messaggi: ["ciao come va?", "malissimo"],
+        messaggi: [
+          {
+            text: "ehi, Andrea",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ehila sono Andrea",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "lo so",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
 
       },
       {
         immagine: "img/avatar_6.jpg",
         nome: "Lucia",
-        messaggi: ["mi piaci", "sono a casa da sola"],
+        messaggi: [
+          {
+            text: "ciao sono Io",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ehila sono Lucia",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "sono contento per te",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
       },
       {
         immagine: "img/avatar_7.jpg",
         nome: "Simone",
-        messaggi: ["piaci a Lucia", "occhio perché ha le piattole", "scherzone xD"],
+        messaggi: [
+          {
+            text: "ciao",
+            stato: "inviato",
+            orario: "12:03 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ehila sono Simone",
+            stato: "ricevuto",
+            orario: "12:04 24 11 2020",
+            data: "24 11 2020"
+          },
+          {
+            text: "ma dai sei Simone!? assurdo!",
+            stato: "inviato",
+            orario: "12:05",
+            data: "24 11 2020"
+          },
+        ],
         dataEora: "23 11 2020 11:58:03",
-        msgSalvati: [],
       },
     ],
     // classi: "visibile, invisible",
-    selectedName: "Paolo",
-    selectedImm: "img/avatar_1.jpg",
-    selectedMsg: ["ciao come va?", "tutto bene grazie"],
     message: '',
     salvali: [],
   },
@@ -76,13 +230,9 @@ var app = new Vue ({
   },
   methods: {
     // toggleClassi: () => { app.classi === "visible" ? app.classi = "invisible" : app.classi = "visible"},
-    selezioneItem(item){
-      this.selectedName = null;
-      this.selectedImm = null;
-      this.selectedMsg = null;
-      this.selectedName = item.nome;
-      this.selectedImm = item.immagine;
-      this.selectedMsg = item.messaggi;
+    selezioneItem(i){
+      // rendo item preso al click attivo
+      this.attivo = i;
     },
     inviaMsg: function () {
       this.salvali.push(this.message);
